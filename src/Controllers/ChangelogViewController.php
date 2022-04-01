9c896cb8e350a091dc9051bde921be5f24af7104
Eduardo Souza <eduardo.dasilva@canoastec.rs.gov.br>
2022-04-01 14:48:49 -0300

    adicionando opção em json

--- src/Controllers/ChangelogViewController.php
+++ src/Controllers/ChangelogViewController.php
@@ -33,4 +33,32 @@ class ChangelogViewController extends BaseController
 
         return view('changelog-view::index', ['changeLog' => $changeLog]);
     }
+    
+    public function json($key = null)
+    {
+        $fileChangelog = fopen(base_path('CHANGE_LOG.txt'), "r");
+
+        $changeLog = [];
+        $isVersion = true;
+        $version = null;
+
+        while(($row = fgets($fileChangelog)) !== false){
+            if($isVersion){
+                $version = trim($row);
+                $isVersion = false;
+                $changeLog[$version] = [];
+            }else if(empty(trim($row))){
+                $isVersion = true;
+            }else{
+                $changeLog[$version][] = $row;
+            }
+        }
+        fclose($fileChangelog);
+
+        if($key && isset($changeLog[$key])){
+            $changeLog = array($key => $changeLog[$key]);
+        }
+
+        return response()->json($changeLog);
+    }
 }
--- src/Routes/web.php
+++ src/Routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/{key?}', 'ChangelogViewController@index')->name('changelog-view.index');
+Route::get('/{key?}/json', 'ChangelogViewController@json')->name('changelog-view.json');
